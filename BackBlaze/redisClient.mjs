2716c73040e39b52e799db411eda43462430a4a7
aks-110 <aksfromup93@gmail.com>
2026-03-22 14:21:06 +0530

    change redis code

--- BackBlaze/redisClient.mjs
+++ BackBlaze/redisClient.mjs
@@ -1,8 +1,7 @@
-import IORedis from 'ioredis';
+import IORedis from "ioredis";
 
-export const connection = new IORedis({
-  host: process.env.REDIS_HOST || "localhost",
-  port: process.env.REDIS_PORT || 6379,
-  password: process.env.REDIS_PASSWORD || undefined,
-  maxRetriesPerRequest: null
+export const connection = new IORedis(process.env.REDIS_URL, {
+  maxRetriesPerRequest: null,
+  tls: {}
 });
+
