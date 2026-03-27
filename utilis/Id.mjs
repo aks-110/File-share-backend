657f19f0d49bedf53490db8c50a57897fa3dbd4b
aks-110 <aksfromup93@gmail.com>
2026-03-27 19:17:56 +0530

    change for deletion

--- utilis/Id.mjs
+++ utilis/Id.mjs
@@ -1,11 +1,12 @@
-const crypto = require("crypto");
+import crypto from "crypto";
+
 const alphabet = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789";
 
- function generateId() {
+export default function generateId() {
   const bytes = crypto.randomBytes(10);
   let result = "";
-  for (let i = 0; i < 10; i++) result += alphabet[bytes[i] & 31];
-  return `${result.slice(0,4)}-${result.slice(4,8)}-${result.slice(8,10)}`;
+  for (let i = 0; i < 10; i++) {
+    result += alphabet[bytes[i] & 31];
+  }
+  return `${result.slice(0, 4)}-${result.slice(4, 8)}-${result.slice(8, 10)}`;
 }
-
-module.exports = generateId;
